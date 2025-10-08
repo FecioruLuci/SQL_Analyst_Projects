6c8887db7f44a4e3fc93cdb96a9b75008d158bbd
Lucian <birsan.lucian04@gmail.com>
2025-10-08 12:30:56 +0300

    Add files via upload

--- firstproject/myproject1.sql
+++ firstproject/myproject1.sql
@@ -19,7 +19,17 @@ CREATE TABLE retailsales(
 
 SELECT *
 FROM retailsales
-WHERE quantiy is NULL;
+WHERE     
+	sale_date IS NULL OR sale_time IS NULL OR customer_id IS NULL OR 
+    gender IS NULL OR age IS NULL OR category IS NULL OR 
+    quantity IS NULL OR price_per_unit IS NULL OR cogs IS NULL;
+    
+DELETE 
+FROM retail_sales
+WHERE 
+    sale_date IS NULL OR sale_time IS NULL OR customer_id IS NULL OR 
+    gender IS NULL OR age IS NULL OR category IS NULL OR 
+    quantity IS NULL OR price_per_unit IS NULL OR cogs IS NULL;
 
 -- how many sales we have
 
