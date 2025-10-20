1e82e2e59f3d3b6a112c1079d57b89371d66b03a
Lucian <birsan.lucian04@gmail.com>
2025-10-20 12:11:17 +0300

    Add files via upload

--- sixthproject/project6.py
+++ sixthproject/project6.py
@@ -1,4 +1,7 @@
 import pandas as pd
+import psycopg2
+import pymysql
+from sqlalchemy import create_engine
 
 df = pd.read_csv(r'C:/Program Files (x86)/vcodestuff/project6\Walmart_SQL_Python/Walmart.csv')
 
@@ -8,6 +11,7 @@ df = pd.read_csv(r'C:/Program Files (x86)/vcodestuff/project6\Walmart_SQL_Python
 #we can see that unit_price has $ sign so it will affect in calculations
 #print(df.duplicated().sum())
 df.dropna(inplace=True)
+df.drop_duplicates(inplace=True)
 #print(df.shape)
 #print(df.isnull().sum())
 df['unit_price'] = df['unit_price'].str.replace('$','').astype(float)
@@ -16,4 +20,17 @@ df['unit_price'] = df['unit_price'].str.replace('$','').astype(float)
 #print(df.info())
 #aswell unit_price is float
 df['total'] = df['unit_price'] * df['quantity']
-print(df.head(10))
+#print(df.head(10))
+#print(df.shape)
+df.to_csv("wallmart_after_python.csv", index=False)
+
+enine_postgresql = create_engine("postgresql+psycopg2://postgres:root@localhost:5432/wallmart")
+try:
+    enine_postgresql
+    print("Connection worked!!")
+except:
+    print("Connection failed!!")
+
+df.columns = df.columns.str.lower()
+#print(df.columns)
+df.to_sql("wallmart",con=enine_postgresql, if_exists="append", index=False)
